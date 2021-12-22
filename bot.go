d19ed7fad089fcb5ec7694b7c89bc2dd9a226fe6
patzar <pzaryjewskI@gmail.com>
2021-12-21 16:22:41 -0800

    Update bot.go

--- bot.go
+++ bot.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
+	"net/http"
 
 	"github.com/boltdb/bolt"
 	"github.com/bwmarrin/discordgo"
@@ -18,9 +19,17 @@ type Bot struct {
 type UserQuotes struct {
 	Quotes   []string
 	Username string
+	QuoteIDs map[string]string
 }
 
 type selector func([]string) (string, int)
+
+func first(arr []*discordgo.MessageEmbed) *discordgo.MessageEmbed {
+	if len(arr) > 0 {
+		return arr[0]
+	}
+	return nil
+}
 
 // Saves quote to the database. Returns true if the save was successful.
 func (b Bot) quoteImpl(user string, text string, messageID string) bool {
@@ -50,9 +59,10 @@ func (b Bot) quoteImpl(user string, text string, messageID string) bool {
 		if len(v) > 0 {
 			json.Unmarshal(v, &userQuote)
 		} else {
-			userQuote = UserQuotes{[]string{}, user}
+			userQuote = UserQuotes{[]string{}, user, map[string]string{}}
 		}
 		userQuote.Quotes = append(userQuote.Quotes, text)
+		userQuote.QuoteIDs[text] = messageID
 		userQuoteSerialized, err := json.Marshal(userQuote)
 
 		err = bucket.Put([]byte(user), []byte(userQuoteSerialized))
@@ -66,19 +76,47 @@ func (b Bot) viewQuote(s *discordgo.Session, m *discordgo.MessageCreate, user st
 	b.db.View(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket([]byte("QuoteBucket"))
 		v := bucket.Get([]byte(user))
+		sent := false
 		if len(v) > 0 {
 			var userQuote UserQuotes
 			json.Unmarshal(v, &userQuote)
 			if len(userQuote.Quotes) > 0 {
 				q, i := fn(userQuote.Quotes)
-				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("> #%d: %s", i, q))
+				qid, ok := userQuote.QuoteIDs[q]
+				fmt.Println(qid)
+				if ok {
+					msg, err := s.ChannelMessage(m.ChannelID, qid)
+					if err == nil {
+						a := msg.Attachments[0]
+						resp, _ := http.Get(a.URL)
+						//defer resp.Body.Close()
+						files := []*discordgo.File{}
+						if err == nil {
+							files = append(files, &discordgo.File{
+								Name:        "blah.png",
+								ContentType: "image/png",
+								Reader:      resp.Body,
+							})
+						}
+						s.ChannelMessageSendComplex(m.ChannelID, &discordgo.MessageSend{
+							Content: fmt.Sprintf("> #%d: %s", i, q),
+							Embed:   first(msg.Embeds),
+							Files:   files,
+						})
+						sent = true
+
+					}
+				}
+				if !sent {
+					s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("> #%d: %s", i, q))
+				}
 			}
 		}
 		return nil
 	})
 }
 
-// Randomquote views a random quote. 
+// Randomquote views a random quote.
 func (b Bot) Randomquote(s *discordgo.Session, m *discordgo.MessageCreate, user string) {
 	b.viewQuote(s, m, user, func(quotes []string) (string, int) {
 		i := rand.Intn(len(quotes))
@@ -86,10 +124,10 @@ func (b Bot) Randomquote(s *discordgo.Session, m *discordgo.MessageCreate, user 
 	})
 }
 
-// Lastquote views a last quote. 
+// Lastquote views a last quote.
 func (b Bot) Lastquote(s *discordgo.Session, m *discordgo.MessageCreate, user string) {
 	b.viewQuote(s, m, user, func(quotes []string) (string, int) {
-		i := len(quotes)-1
-		return quotes[i], i 
+		i := len(quotes) - 1
+		return quotes[i], i
 	})
 }
